e3b7fdc871c35eb77fa27e32749477af7fa3c25d
fabidesca <fabiandescampe@hotmail.com>
2017-12-18 09:15:10 +0100

    Ajout Javadoc agence

--- model/Agence.java
+++ model/Agence.java
@@ -1,14 +1,29 @@
 package model;
+/**
+ * Cette classe implemente une agence avec un ID, une ville et une adresse
+ * @author fabia
+ *
+ */
 public class Agence {
 	private int agenceID;
 	private String ville;
 	private String adresse;
+	/**
+	 * Ce constructeur creer une agence en specifiant sa ville et son adresse
+	 * @param ville de l'agence
+	 * @param adresse de l'agence
+	 */
 	
 	public Agence(String ville, String adresse) {
 		this.ville = ville;
 		this.adresse = adresse;
 	}
-	
+	/**
+	 * Ce constructeur creer une agence avec ID specifique, une ville et son adresse
+	 * @param agenceID specifique a chaque agence
+	 * @param ville de l'agence
+	 * @param adresse de l'agence
+	 */
 
 	public Agence(int agenceID, String ville, String adresse) {
 		super();
@@ -16,38 +31,57 @@ public class Agence {
 		this.ville = ville;
 		this.adresse = adresse;
 	}
-
+	/**
+	 * Permet de recuperer l'ID de l'agence
+	 * @return l'ID de l'agence a obtenir
+	 */
 
 	public int getAgenceID() {
 		return agenceID;
 	}
+	/**
+	 * 
+	 * @param agenceID l'ID de l'agence a mettre
+	 */
 
 	public void setAgenceID(int agenceID) {
 		this.agenceID = agenceID;
 	}
-
+	/**
+	 * Obtiens la ville de l'agence
+	 * @return
+	 */
 	public String getVille() {
 		return ville;
 	}
-
+	/**
+	 * 
+	 * @param ville de l'agence a mettre
+	 */
 	public void setVille(String ville) {
 		this.ville = ville;
 	}
-
+	/**
+	 * Obtiens l'adresse de l'agence
+	 * @return
+	 */
 	public String getAdresse() {
 		return adresse;
 	}
-
+	/**
+	 * 
+	 * @param adresse de l'agence a mettre
+	 */
 	public void setAdresse(String adresse) {
 		this.adresse = adresse;
 	}
+	/**
+	 * Cette methode permet d'afficher en chaine de caracteres les differents parametres d'une agence 
+	 */
 
-	@Override
 	public String toString() {
 		return "agenceID=" + agenceID + ", ville=" + ville + ", adresse=" + adresse;
 	}
-	
-	
 
 }
 
